7c733a4ba51ec5c8ddefe4419c41f089b5820b96
lain <sandrokalett@gmail.com>
2022-02-08 19:45:29 +0100

    minor

--- lucs_ez/log.py
+++ lucs_ez/log.py
@@ -1,6 +1,6 @@
 ########################################################################################################################
 #                                                   Lainupcomputer                                                     #
-#                                                   ez_log v1.0.1                                                      #
+#                                                   ez_log v1.0.2                                                      #
 ########################################################################################################################
 import datetime
 
@@ -9,7 +9,7 @@ class log:
 
     def write_init(self, opt):
         with open(self.filepath, "w") as f:
-            f.write(f"> lucs_ez logfile ({opt}) <\n")
+            f.write(f"{datetime.datetime.now()} : logfile ({opt}) <\n")
 
     def __init__(self, destination="log.txt", new=True):
         self.filepath = destination
@@ -33,4 +33,5 @@ class log:
     def log(self, msg="blanc"):
         logstr = f"{datetime.datetime.now()} : {msg}\n"
         lines = self.internal_read()
-        self.internal_write(lines.append(logstr))
+        lines.append(logstr)
+        self.internal_write(lines)
