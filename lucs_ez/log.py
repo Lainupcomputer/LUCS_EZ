34fa6d6bfaac79fbce9386ba328d15a60f5a09aa
lain <sandrokalett@gmail.com>
2022-02-08 19:31:07 +0100

    changed init added option not to override

--- lucs_ez/log.py
+++ lucs_ez/log.py
@@ -7,10 +7,20 @@ import datetime
 
 class log:
 
-    def __init__(self, destination="log.txt"):
-        self.filepath = destination
+    def write_init(self, opt):
         with open(self.filepath, "w") as f:
-            f.write("> lucs_ez logfile <\n")
+            f.write(f"> lucs_ez logfile ({opt}) <\n")
+
+    def __init__(self, destination="log.txt", new=True):
+        self.filepath = destination
+        if new:
+            self.write_init("temporary")
+        else:
+            try:
+                self.internal_read()
+
+            except FileNotFoundError:
+                self.write_init("static")
 
     def log(self, msg="blanc"):
         logstr = f"{datetime.datetime.now()} : {msg}\n"
